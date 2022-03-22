0e0108fc1f5b1b999306ce2d2abb0b87374e0dec
吴鑫磊 <goldbridge_wuxl55@126.com>
2022-03-22 14:47:40 +0800

    初始化项目

---
+++ DYZB/DYZB/DYZBApp.swift
@@ -0,0 +1,17 @@
+//
+//  DYZBApp.swift
+//  DYZB
+//
+//  Created by wuxl on 2022/3/22.
+//
+
+import SwiftUI
+
+@main
+struct DYZBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
